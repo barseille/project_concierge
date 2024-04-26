c0518aeba84ee302ef1a98bc194d14858bb359d9
BarseillePonn <ponn.barseille@gmail.com>
2024-04-26 23:51:14 +0200

    Update static files configuration

--- config/settings.py
+++ config/settings.py
@@ -152,9 +152,10 @@ AUTH_USER_MODEL = 'authentication.User'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 LANGUAGES = [
