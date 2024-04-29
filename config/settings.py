6b5857089efe1e888cdbd101c7fd277ff67ae765
BarseillePonn <ponn.barseille@gmail.com>
2024-04-29 12:25:15 +0200

    correction erreur formulaire

--- config/settings.py
+++ config/settings.py
@@ -1,23 +1,24 @@
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-import dj_database_url
-import django_heroku
 
 # Charge les variables d'environnement du fichier .env
 load_dotenv()
 
-# Définition du chemin de base du projet
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Clé secrète et mode débogage
+# DEBUG = True
+
+# Accéder à une variable d'environnement
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv('DEBUG') == 'True'
 
-# Hôtes autorisés
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
-# Applications Django et applications tierces
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -29,15 +30,13 @@ INSTALLED_APPS = [
     "theme",
     "authentication",
     "blog",
-    "modeltranslation",  # Gestion des traductions des modèles
+    "modeltranslation",
 ]
 
-# Middleware utilisé par l'application
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",  # Gestion des fichiers statiques
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.locale.LocaleMiddleware",  # Gestion des langues
+    "django.middleware.locale.LocaleMiddleware",
     "middleware.AdminLocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -46,10 +45,10 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-# Configuration de l'URL principale
+
+
 ROOT_URLCONF = "config.urls"
 
-# Configuration des templates
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
@@ -67,35 +66,82 @@ TEMPLATES = [
     },
 ]
 
-# Configuration de l'application WSGI
 WSGI_APPLICATION = "config.wsgi.application"
 
-# Configuration de la base de données selon l'environnement
-ENV = os.getenv('ENV', 'development')
-if ENV == 'production':
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=f"postgres://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}",
-            conn_max_age=600,
-            ssl_require=True
-        )
-    }
-else:
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=f"postgres://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}",
-            conn_max_age=600,
-            ssl_require=False
-        )
-    }
 
-# Configuration des fichiers statiques
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+DATABASES = {
+    "default": {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
+}
 
-# Configuration de l'envoi d'emails
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = "static/"
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+TAILWIND_APP_NAME = 'theme'
+
+NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
+
+AUTH_USER_MODEL = 'authentication.User'
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+
+LANGUAGES = [
+    ('en', 'en'),
+    ('fr', 'fr'),
+]
+
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale'),
+)
+
+
 EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
 EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
@@ -103,28 +149,8 @@ EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
-# Configuration des langues et de la localisation
-LANGUAGE_CODE = "en-us"
-TIME_ZONE = "UTC"
-USE_I18N = True
-USE_TZ = True
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-INTERNAL_IPS = ["127.0.0.1"]
-TAILWIND_APP_NAME = 'theme'
-NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
-AUTH_USER_MODEL = 'authentication.User'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# Chemin du système de fichiers où les fichiers téléchargés seront stockés.
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# URL publique de base pour servir les fichiers média téléchargés.
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-
-# Langues disponibles
-LANGUAGES = [
-    ('en', 'English'),
-    ('fr', 'Français'),
-]
-
-# Chemins des fichiers de traduction
-LOCALE_PATHS = (os.path.join(BASE_DIR, 'locale'),)
-
-# Configuration django-heroku pour paramétrer automatiquement l'application pour Heroku
-django_heroku.settings(locals())
